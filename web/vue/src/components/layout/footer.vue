3e723fe9f7cbae5800522bea8c02bf6991aecfcb
viditkhemka63 <viditkhemka63@gmail.com>
2019-10-14 16:00:52 +0530

    initial commit

---
+++ web/vue/src/components/layout/footer.vue
@@ -0,0 +1,23 @@
+<template lang='pug'>
+  footer.p2.bg--off-white
+    .contain
+      p
+        em A deep reinforcement learning project
+      p 
+</template>
+
+<script>
+const gekkoPackage = require('../../../../../package.json');
+const uiPackage = require('../../../package.json');
+
+export default {
+  data: () => {
+    return {
+      version: {
+        gekko: gekkoPackage.version,
+        ui: uiPackage.version
+      }
+    }
+  }
+}
+</script>
---
+++ web/vue/src/components/layout/home.vue
@@ -0,0 +1,30 @@
+<template lang='pug'>
+  section.contain.grd-row
+    .grd-row-col-3-6(v-html='left')
+    .grd-row-col-3-6.txt--center
+      img(src='static/gekko.jpg')
+      p
+        em The most valuable commodity I know of is information.
+</template>
+
+<script>
+import marked from '../../tools/marked';
+
+const left = marked(`
+
+## Gradient Trader
+
+It is a Bitcoin trading bot and backtesting platform that
+connects to popular Bitcoin exchanges. It is written in javascript
+and runs on nodejs.
+
+`);
+
+export default {
+  data: () => {
+    return {
+      left
+    }
+  }
+}
+</script>
